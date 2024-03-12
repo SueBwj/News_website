13a31a462b78d22e629b7e56aaa85ad9c80c6760
Sue <1175634156@qq.com>
2024-03-12 20:35:47 +0800

    添加了Navbar功能

--- news_website/src/App.vue
+++ news_website/src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 import Body from "./components/Body.vue";
 import Comments from "./components/Comments.vue";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
   <div class="container">
-    <div class="row">
+    <div class="row mb-5">
+      <Navbar />
+    </div>
+    <div class="row mt-3">
       <Body />
     </div>
     <div class="row">
---
+++ news_website/src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="navbar fixed-top bg-body-tertiary shadow-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">News Project</a>
+    </div>
+  </nav>
+</template>
