f3d51d469b3b20e0457e28e16324f0c8b3d79fd5
Sue <1175634156@qq.com>
2024-03-12 19:11:50 +0800

    RightSide version-1

--- news_website/src/components/RightSide.vue
+++ news_website/src/components/RightSide.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container border border-2 rounded-4 shadow-sm p-3 mb-5">
     <nav class="navbar bg">
       <div class="container-fluid">
         <form class="d-flex" role="search">
@@ -13,7 +13,17 @@
         </form>
       </div>
     </nav>
-    <label for="customRange2" class="form-label">Example range</label>
+    <label for="customRange2" class="form-label mb-0"
+      ><p class="fs-6 fw-light mb-0">ratio of text to comment</p></label
+    >
     <input type="range" class="form-range" min="0" max="5" id="customRange2" />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, autem
+      quis. Animi sunt labore id veniam tempora adipisci aspernatur quam facere
+      sequi eos totam, iusto praesentium voluptates. Mollitia, consectetur
+      reprehenderit! Libero pariatur cum temporibus magnam, commodi itaque iste
+      esse necessitatibus repellendus obcaecati maxime et totam eveniet
+      praesentium vitae facilis dolores!
+    </p>
   </div>
 </template>
