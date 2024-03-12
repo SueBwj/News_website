00300244c36ae72f5b00d11822e8337ef79df023
Sue <1175634156@qq.com>
2024-03-12 20:23:35 +0800

    增加评论样例

--- news_website/src/components/Comments.vue
+++ news_website/src/components/Comments.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="container">
-    <h6>This is comment part</h6>
+  <div class="container mt-4 p-2 border-top border-3">
+    <h4>This is Comment Part</h4>
+    <img src="../assets/img/评论样例.jpg" class="img-fluid" alt="评论样例" />
   </div>
 </template>
