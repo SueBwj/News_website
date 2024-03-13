b4bee49940877c154fcda04b718e898151b726dc
SueBwj <118652443+SueBwj@users.noreply.github.com>
2024-03-13 21:27:01 +0800

    Delete backend/key_param.py

--- backend/key_param.py
+++
@@ -1,2 +0,0 @@
-openai_api_key = "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"
-MONGO_URI = "mongodb+srv://panqy27:200610088Pqy@cluster0.1uw2sds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
